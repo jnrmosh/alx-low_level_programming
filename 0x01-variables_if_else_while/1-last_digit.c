9a5e48c6e6a4cac4a238455279ddbd75ef9d0401
jnrmosh <omondimichaelotieno@gmail.com>
2022-07-01 11:42:10 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,11 @@ int main(void)
 	}
 	else if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf(" and is less than 6 and 0\n");
+		printf(" and is less than 6 and not 0\n");
+	}
+	else
+	{
+		printf(" is 0 and is 0\n");
 	}
 	return (0);
 }
