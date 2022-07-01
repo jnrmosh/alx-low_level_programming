35a1a4ae365fb7ccce2423b752f8fb12eb8ea29d
jnrmosh <omondimichaelotieno@gmail.com>
2022-07-01 13:31:28 -0700

    print base 10 numbers between 0 and 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Decsription - print all single digit numbers of base 10 starting from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
