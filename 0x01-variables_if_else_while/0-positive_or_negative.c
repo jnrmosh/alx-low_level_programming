12bfd55fe6d208361f6160acf79e9410e13f3799
jnrmosh <omondimichaelotieno@gmail.com>
2022-07-01 10:35:25 -0700

    Assign random number to a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * Main - assigns a radnom number to int everytime
+ * Return: Always 0 success
+ */
 int main(void)
 {
 	int n;
@@ -12,15 +14,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("is zero");
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("is negative");
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
