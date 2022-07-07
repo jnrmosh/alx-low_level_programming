026282c48a78bfacc34f6a8dc6375337f54d3d3e
jnrmosh <omondimichaelotieno@gmail.com>
2022-07-07 15:08:37 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers between 0 and 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		_putchar(K + '0');
+	}
+	_putchar('\n');
+}
