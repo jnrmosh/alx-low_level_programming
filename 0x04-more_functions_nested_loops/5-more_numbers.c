10b709bd8244ff43165477df521291532f647986
jnrmosh <omondimichaelotieno@gmail.com>
2022-07-07 15:55:32 -0700

    print numbers between 0 and 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - print numbers between 0 and 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i > 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+				_putchar(i % 10 + '0');
+			i++;
+		}
+	_putchar('\n');
+	}
+}
