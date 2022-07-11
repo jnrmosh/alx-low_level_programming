6df568f5f3f2d70eb6032f2091633e43a36febce
jnrmosh <omondimichaelotieno@gmail.com>
2022-07-11 09:01:59 -0700

    prints one character out of 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @s: input character
+ * Return: No return
+ */
+void puts2(char *str)
+{
+	int i, j = 0;
+
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j = j + 2;
+	}
+	_putchar('\n');
+}
