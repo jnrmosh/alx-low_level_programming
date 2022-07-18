ceac1f91a33e357906e6bbf4d06115332d2d44df
jnrmosh <omondimichaelotieno@gmail.com>
2022-07-18 11:46:52 -0700

    get length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+/**
+ * _strspn - Gets length of a prefix substring
+ * @s: String where substring will look
+ * @accept: accepted substring
+ * Return: Legth of occurence
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while (*s++)
+	{
+		while (*accept)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+	return (c);
+}
